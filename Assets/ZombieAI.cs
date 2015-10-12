f4a7983e79831e5bc4fc75584d800c7e3967c99e
Philipp Mascha <p.mascha@gmx.net>
2015-10-12 12:24:55 +0200

    Initial

---
+++ Assets/ZombieAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAI : MonoBehaviour {
+
+    private int x, y;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
