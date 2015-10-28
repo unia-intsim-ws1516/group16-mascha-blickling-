846ed19093db75b52c737444f1e135549110bf98
putrick <Patrick.blickling@hotmail.de>
2015-10-28 13:42:36 +0100

    Inserted EnvironmentInitializer.cs
    initial commit of the environment and all Navigation-purposes

---
+++ Assets/MoveToPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToPoint : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Transform target;
+    private bool onItsWay = false;
+    private NavMeshAgent agent;
+    private int visited;
+
+    void Start()
+    {
+        target = GameObject.Find("Zombie").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Updates destination every frame. Now this agent is walking via NavMesh (parameters via NavAgentComponent)
+        agent.SetDestination(target.position);
+    }
+}
