7a9a76e4c12f20fdd0f807cc124f24551eaa2504
DerMascha <p.mascha@gmx.net>
2015-11-03 12:55:08 +0100

    Disease (missed)

---
+++ Assets/Disease.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    class Disease : MonoBehaviour
+    {
+        public float SpreadChance = 0.02F;
+        public float HealChance = 0.2F;
+        public int HappinesDecrease = 1;
+
+        public int BoredomIncrease = 1;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+    }
+}
